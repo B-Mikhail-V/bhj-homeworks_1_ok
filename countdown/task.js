05195f7c1e0c4fd02a076e1b8af7cdf68d6b1b68
B-Mikhail-V <bakulin.mv@gmail.com>
2022-09-06 23:29:18 +0300

    Commit_2

--- countdown/task.js
+++ countdown/task.js
@@ -2,8 +2,6 @@ const count = document.getElementById('timer')
 const count2 = document.getElementById('timer2')
 secondsAll2 = Number(count.textContent);
 nIntervalID = setInterval(() => {
-    // secondsAll = Number(count.textContent);
-    // secondsAll2 = secondsAll
     secondsAll2 -= 1
     hours = Math.floor(secondsAll2 / 3600);
     minutes = Math.floor((secondsAll2 - hours * 3600) / 60);
@@ -16,11 +14,6 @@ nIntervalID = setInterval(() => {
     count2.textContent = hoursPrint + minutesPrint + secondsPrint
 
     console.log(hoursPrint + minutesPrint + secondsPrint);
-
-
-    // console.log(('0' + hours + ':').slice(-2));
-    // console.log(('0' + minutes + ':').slice(-2));
-    // console.log(('0' + seconds).slice(-2));
 
     if (secondsAll2 <= 0) {  
         alert("Вы победили в конкурсе!");
